fddd8e479ed6463bfdbf6b0d5891bccbb557e702
CatBackmanCasino <eric.aj.backman@gmail.com>
2022-04-07 16:19:25 +0000

    add welcome function to greet player

---
+++ run.py
@@ -0,0 +1,30 @@
+scales_dict = [{
+    "scale": "Major",
+    "index": [0, 2, 4, 5, 7, 9, 11]
+}, {
+    "scale": "Natural Minor",
+    "index": [0, 2, 3, 5, 7, 8, 10]
+}]
+
+def welcome():
+    """
+    Welcomes the player and starts the game.
+    """
+    print("Welcome to ScaleTrainer 1.0 BETA\n"
+          "\n"
+          "If you wish to know your scales this game is for you.\n"
+          "\n"
+          "Choose the scale you wish to practice\n"
+          "then the Game will randomize a key and you will be asked to enter\n"
+          "the notes for that scale in a random key.\n"
+          "\n"
+          "\n"
+          "\n"
+          "Game instructions:\n"
+          "1. Choose scale\n"
+          "2. Enter all the notes of that scale in the key provided\n"
+          "3. Your result is shown\n"
+          "4. play again!"
+          "\n")
+
+welcome()
